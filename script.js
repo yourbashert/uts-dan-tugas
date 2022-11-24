8ec82d10c8cb3315b40a1e722895eb21e6b92839
yourbashert <imandatmrp1000@gmail.com>
2022-11-24 11:07:36 +0700

    complete website for uts and tugas

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ window.onscroll = function () {
         nav.classList.remove('activated');
     }
 }
+document.querySelectorAll('#nav-tab>[data-bs-toggle="tab"]').forEach(el => {
+  el.addEventListener('shown.bs.tab', () => {
+    const target = el.getAttribute('data-bs-target')
+    const scrollElem = document.querySelector(`${target} [data-bs-spy="scroll"]`)
+    bootstrap.ScrollSpy.getOrCreateInstance(scrollElem).refresh()
+  })
+})
+
+const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#list-example'
+})
